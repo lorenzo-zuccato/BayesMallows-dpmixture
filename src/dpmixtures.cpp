71c89a81c4925b51176eefb67e193f610ce9b936
Lorenzo Zuccato <lorenzo.zuccato99@gmail.com>
2023-10-09 09:45:30 +0200

    Completed initial cluster assignment functions

--- src/dpmixtures.cpp
+++ src/dpmixtures.cpp
@@ -7,21 +7,29 @@ using namespace arma;
 
 // [[Rcpp::depends(RcppArmadillo)]]
 
+
 uvec initialize_cluster_assignment(int n_assessors, int psi){
     uvec cluster_assignment(n_assessors);
     cluster_assignment(0) = 0;
+    int n_clus = 1;
     for(int i = 1; i < n_assessors; ++i){
-        vec prob = initial_assignment_prob(cluster_assignment.subvec(0,i-1), psi);
-
+        n_clus = cluster_assignment.subvec(0, i - 1).max() + 1;
+        vec assignment_prob = initial_assignment_prob(cluster_assignment.subvec(0, i - 1), psi, n_clus);
+        cluster_assignment(i) = sample(regspace<uvec>(0, n.clus), 1, false, assignment_prob);
     }
+    return cluster_assignment;
 }
 
-vec initial_assignment_prob(const uvec already_assigned, int psi){
-    int n_clusters = already_assigned.max()+1;
-    vec prob(n_clusters+1);
+vec initial_assignment_prob(const uvec already_assigned, int psi, int n_clus){
+    vec prob(n_clus + 1);
+    //for(int i = 0; i < n_clusters; ++i){
+    //    prob(i) = find(already_assigned == i).n_elem;
+    //}
+    //prob(n_clus) = psi;
+    //return normalise(prob, 1);
     for(int i = 0; i < n_clusters; ++i){
-        prob(i) = find(already_assigned == i).n_elem;
+        prob(i) = find(already_assigned == i).n_elem / (i + psi);
     }
-    prob(n_clusters) = psi;
-    return normalise(prob, 1);
+    prob(n_clus) = psi / (i + psi);
+    return prob;
 }
