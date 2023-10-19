2262fca677497d2a0161b2f140ba29a59437fe6c
Lorenzo Zuccato <lorenzo.zuccato99@gmail.com>
2023-10-19 14:41:04 +0200

    Updated heaer dpmixture.h

--- src/dpmixtures.h
+++ src/dpmixtures.h
@@ -5,4 +5,30 @@
 
 arma::vec initial_assignment_prob(const arma::uvec already_assigned, const int psi, const int n_clus);
 arma::uvec initialize_cluster_assignment(const int n_assessors, const int psi);
+
+int cluster_count_excluding_i(const arma::uvec& current_cluster_assignment,
+                              const unsigned int& cluster_index,
+                              const int& n_assessors,
+                              const int& i);
+double  log_factorial(const int n);
+
+arma::uvec update_cluster_labels_dpmixture(const arma::mat& rankings
+                                    const arma::vec& obs_freq,
+                                    arma::cube& rho,
+                                    arma::mat& rho_old,
+                                    arma::mat& alpha,
+                                    arma::vec& alpha_old,
+                                    const arma::uvec& current_cluster_assignment,
+                                    arma::uvec& current_clusters,
+                                    int& current_n_clusters,
+                                    int& max_cluster_index,
+                                    const int& n_items,
+                                    const double& log_fact_n_items,
+                                    const double& lambda,
+                                    const double& alpha_max,
+                                    const int& psi,
+                                    const int& leap_size,
+                                    const std::string& metric,
+                                    const Rcpp::Nullable<arma::vec> cardinalities = R_NilValue,
+                                    const Rcpp::Nullable<arma::vec> logz_estimate = R_NilValue);
 #endif
